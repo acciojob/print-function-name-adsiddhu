65ae94e1a36ec675f01a25b33040c8a17d5de2bf
Adarsh Singh Siddhu <111896979+adsiddhu@users.noreply.github.com>
2023-12-11 09:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
-function AccioJob(){
-	console.log(arguments.callee.name)
+function functionName(){
+	alert(arguments.callee.name)
 }
-alert("Acciojob");
-AccioJob()
+functionName();
+// alert("Acciojob");
+// AccioJob()
